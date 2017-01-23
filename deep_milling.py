c2b3ca41b0fd858221166e5e7a0a7af5c51c149f
Erik Strand <erik@plethora.com>
2017-01-22 17:30:23 -0800

    Simplify training and evaluation procedures

--- deep_milling.py
+++ deep_milling.py
@@ -3,11 +3,9 @@ import tensorflow as tf
 tf.set_random_seed(2016)
 
 # Training hyperparameters
-training_cycles = 100000
-print_interval = 1000
-memory_capacity = 50000 # samples in experience memory
-memory_initial = 15000
-actions_per_update = 20
+training_episodes = 100000
+memory_capacity = 100000 # transitions in experience memory
+memory_initial = 25000
 minibatch_size = 64
 target_network_decay = 0.99
 learning_rate = 0.001
@@ -15,107 +13,11 @@ learning_rate = 0.001
 # Q learning hyperparameters
 gamma = 0.99
 epsilon_0 = 1.0
-epsilon_1 = 0.1
-epsilon_ramp = 90000.0
+epsilon_1 = 0.01
+epsilon_ramp = 75000.0
 
 # Evaluation hyperparameters
 evaluation_episodes = 500
-
-class TrainingAgent:
-    def __init__(self):
-        self.environment = Environment()
-        self.reset()
-
-    def reset(self):
-        self.observation = self.environment.reset()
-        self.reset_stats()
-
-    def reset_stats(self):
-        self.actions = 0
-        self.max_cumulative_reward = self.environment.remaining_stock_blocks()
-        self.cumulative_reward = 0.0
-        self.q_actions = 0
-        self.random_actions = 0
-        self.repeat_attempts = 0
-        self.repeats = 0
-
-    def advance(self, epsilon, evaluator):
-        part = self.environment.part
-        s = self.environment.state
-        if s in self.environment.transitions:
-            explored_actions = self.environment.transitions[s]
-        else:
-            explored_actions = set()
-        unexplored_actions = set([0, 1, 2, 3]) - explored_actions
-        new = True
-        if len(unexplored_actions) == 0:
-            new = False
-            self.repeats += 1
-            self.random_actions += 1
-            a = random.randint(0, 3)
-        elif random.random() < epsilon:
-            self.random_actions += 1
-            a = random.sample(unexplored_actions, 1)[0]
-        else:
-            a = evaluator(self.observation)
-            if a in unexplored_actions:
-                self.q_actions += 1
-            else:
-                self.repeat_attempts += 1
-                a = random.sample(unexplored_actions, 1)[0]
-        self.observation, r, done, info = self.environment.step(a)
-        self.actions += 1
-        self.cumulative_reward += r
-        return a, r, new, done
-
-
-class EvaluationAgent:
-    def __init__(self):
-        self.environment = Environment()
-
-    def reset(self):
-        self.observation = self.environment.reset()
-        self.actions = 0
-        self.max_cumulative_reward = self.environment.remaining_stock_blocks()
-        self.cumulative_reward = 0.0
-        self.q_actions = 0
-        self.random_actions = 0
-        self.repeat_attempts = 0
-        self.repeats = 0
-
-    def advance(self, evaluator):
-        a = evaluator(self.observation)
-        self.observation, r, done, info = self.environment.step(a)
-        self.actions += 1
-        self.cumulative_reward += r
-        if info.deja_vu:
-            done = True
-        return done
-
-
-class StatAggregator:
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.episodes = 0
-        self.actions = 0
-        self.q_actions = 0
-        self.random_actions = 0
-        self.repeat_attempts = 0
-        self.repeats = 0
-        self.cumulative_reward = 0.0
-        self.max_cumulative_reward = 0.0
-
-    def update(self, session):
-        self.episodes += 1
-        self.actions += session.actions
-        self.q_actions += session.q_actions
-        self.random_actions += session.random_actions
-        self.repeat_attempts += session.repeat_attempts
-        self.repeats += session.repeats
-        self.cumulative_reward += session.cumulative_reward
-        self.max_cumulative_reward += session.max_cumulative_reward
 
 
 class MemoryBank:
@@ -152,6 +54,8 @@ def relu_layer_with_ema(scope_name, input1, input2, n_inputs, n_outputs, ema, li
         else:
             output = tf.nn.relu(tf.matmul(input1, weights) + biases, name=scope.name)
             output_ema = tf.nn.relu(tf.matmul(input2, weights_ema) + biases_ema, name=ema_name)
+        tf.summary.histogram(scope_name + " histogram", output)
+        tf.summary.histogram(ema_name + " histogram", output_ema)
         return output, output_ema, update_ema
 
 
@@ -168,6 +72,8 @@ class Model:
         self.action = tf.placeholder(tf.int32, [minibatch_size])
         self.reward = tf.placeholder(tf.float32, [minibatch_size])
         self.not_done = tf.placeholder(tf.float32, [minibatch_size])
+        
+        self.score = tf.placeholder(tf.float32, [1])
 
         # Actual feed tensors
         XX_q = tf.reshape(self.X_q, [-1, 2 * W * H])
@@ -200,12 +106,13 @@ class Model:
         self.memory = MemoryBank(memory_capacity)
 
         # Summary stats
-        average_reward = tf.reduce_mean(q_reward)
-        tf.summary.scalar("expected reward (q)", average_reward)
+        tf.summary.scalar("expected reward (q)", tf.reduce_mean(q_reward))
+        tf.summary.scalar("expected reward (t)", tf.reduce_mean(t_reward))
+        tf.summary.scalar("expected_squared_error", expected_squared_error)
         self.summaries = tf.summary.merge_all()
 
     def train(self, sess):
-        summary_writer = tf.train.SummaryWriter("./train", sess.graph)
+        summary_writer = tf.summary.FileWriter("./train", sess.graph)
 
         # Seed memory bank
         env = Environment()
@@ -219,25 +126,19 @@ class Model:
                 prior_obs = post_obs
 
         epsilon = epsilon_0
-        train_agent = TrainingAgent()
-        eval_agent = EvaluationAgent()
-        train_stats = StatAggregator()
-        eval_stats = StatAggregator()
         evaluator = lambda s: sess.run(self.best_action, feed_dict = {self.X_q: [s]})[0]
-        train_agent.reset()
-        for i in range(1, training_cycles + 1):
+        for i in range(0, training_episodes):
             # Generate new state/action pairs according to the current Q function
-            new_transitions = 0
-            while new_transitions < actions_per_update:
-                prior_observation = train_agent.observation
-                a, r, new, done = train_agent.advance(epsilon, evaluator)
-                post_observation = train_agent.observation
-                if new:
-                    new_transitions += 1
-                    self.memory.store(prior_observation, a, r, post_observation, done)
-                if done:
-                    train_stats.update(train_agent)
-                    train_agent.reset()
+            prior_obs = env.reset()
+            done = False
+            while not done:
+                if random.random() < epsilon:
+                    a = random.randint(0, 3)
+                else:
+                    a = evaluator(prior_obs)
+                post_obs, reward, done, info = env.step(a)
+                self.memory.store(prior_obs, a, reward, post_obs, done)
+                prior_obs = post_obs
 
             # Get a sample
             sample = self.memory.sample(minibatch_size)
@@ -263,43 +164,36 @@ class Model:
                 ramp_completion = float(i) / epsilon_ramp
                 epsilon = (1.0 - ramp_completion) * epsilon_0 + ramp_completion * epsilon_1
 
-            summary_writer
-
-            # Print an update if applicable
-            if i % print_interval == 0:
-                eval_agent.reset()
-                for _ in range(0, evaluation_episodes):
+            if i % 1000 == 0:
+                # Perform an evaluation run (no random actions)
+                test_episodes = 10
+                actions = np.zeros((test_episodes))
+                scores = np.zeros((test_episodes))
+                possible_scores = np.zeros((test_episodes))
+                for j in range(0, 10):
+                    prior_obs = env.reset()
                     done = False
+                    possible_scores[j] = env.remaining_stock_blocks()
                     while not done:
-                        done = eval_agent.advance(evaluator)
-                    eval_stats.update(eval_agent)
-                    eval_agent.reset()
-
-                print("================================================================================")
-                print("Epsilon = " + str(epsilon))
-
-                summary = "Completed " + str(train_stats.episodes) + " episodes with " + str(train_stats.actions) + " total transitions ("
-                summary += str(train_stats.repeat_attempts) + " repeat attempts, "
-                summary += str(train_stats.repeats) + " forced repeats, "
-                summary += str(float(train_stats.random_actions)/train_stats.actions*100) + "% random actions)"
-                print(summary)
-                print("Average reward fraction: " + str(float(train_stats.cumulative_reward)/train_stats.max_cumulative_reward) + " (of " + str(train_stats.max_cumulative_reward) + " possible rewards)")
-                print("Average steps to completion: " + str(float(train_stats.actions)/train_stats.episodes))
-                train_stats.reset()
-
-                summary = "Completed " + str(eval_stats.episodes) + " episodes with " + str(eval_stats.actions) + " total transitions ("
-                summary += str(eval_stats.repeat_attempts) + " repeat attempts, "
-                summary += str(eval_stats.repeats) + " forced repeats, "
-                summary += str(float(eval_stats.random_actions)/eval_stats.actions*100) + "% random actions)"
-                print(summary)
-                print("Average reward fraction: " + str(float(eval_stats.cumulative_reward)/eval_stats.max_cumulative_reward) + " (of " + str(eval_stats.max_cumulative_reward) + " possible rewards)")
-                print("Average steps to completion: " + str(float(eval_stats.actions)/eval_stats.episodes))
-                eval_stats.reset()
+                        a = evaluator(prior_obs)
+                        post_obs, reward, done, info = env.step(a)
+                        self.memory.store(prior_obs, a, reward, post_obs, done)
+                        prior_obs = post_obs
+                        actions[j] += 1
+                        scores[j] += reward
+                total_actions = np.sum(actions)
+                total_score = np.sum(scores)
+                total_possible_score = np.sum(possible_scores)
+                score_ratios = scores / possible_scores
+                print("Episode " + str(i) + ": epsilon = " + str(epsilon))
+                print("  Performed " + str(total_actions) + " total actions, average of " + str(np.mean(actions)) + " per episode")
+                print("  Scored " + str(total_score) + " of " + str(total_possible_score) + " (" + str(100*total_score/total_possible_score) + "%)")
+                print("  Average score per episode: " + str(np.mean(scores)) + ", average score ratio per episode: " + str(np.mean(score_ratios)))
 
 
 if __name__ == "__main__":
     model = Model()
-    init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
     sess = tf.Session()
     sess.run(init)
     model.train(sess)
